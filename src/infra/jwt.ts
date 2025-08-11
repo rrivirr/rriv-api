fdded16a384c69c1255c19b0845c328cf77a54ae
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2025-08-11 09:39:36 +0100

    fix: update keycloak issuer

--- src/infra/jwt.ts
+++ src/infra/jwt.ts
@@ -1,8 +1,11 @@
 // @deno-types="npm:@types/jsonwebtoken@9"
 import jwt from "npm:jsonwebtoken";
+import { keycloakIssuer } from "./keycloak/config.ts";
 
 export const verifyJwtToken = (token: string) => {
   const publicKey = Deno.env.get("KEYCLOAK_PUBLIC_KEY");
-  const issuer = Deno.env.get("KEYCLOAK_ISSUER");
-  return jwt.verify(token, publicKey!, { algorithms: ["RS256"], issuer });
+  return jwt.verify(token, publicKey!, {
+    algorithms: ["RS256"],
+    issuer: keycloakIssuer,
+  });
 };
