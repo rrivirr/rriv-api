697486a3ac975441e20c7349443c6dd8b129c237
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-03-12 15:23:01 +0100

    fix: trigger release

--- src/winston.ts
+++ src/winston.ts
@@ -1,7 +1,6 @@
 import winston from "npm:winston";
 
 const logLevel = Deno.env.get("LOG_LEVEL") ?? "info";
-
 const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp(),
